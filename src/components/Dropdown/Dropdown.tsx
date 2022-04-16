adc2d0f850e0c47b0ddca814c2c16651f0f21baa
AnJuHwan <wnghks0531@gmail.com>
2022-04-16 22:03:31 +0900

    dropdown 완성

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import { faSearch, faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import CenterContainer from '../CenterContainer/CenterContainer';
+import styles from './Dropdown.module.css';
 
 const Dropdown = () => {
+  const dropdownItems: string[] = [
+    'All Symbols',
+    'BTCUSD.PERP',
+    'ETHUSD.PERP',
+    'BCHUSD.PERP',
+    'LTCUSD.PERP',
+    'XRPUSD.PERP',
+  ];
+  const [openDropdown, setOpenDropdown] = useState<boolean>(false);
+  const [dropdownList, setDropdownList] = useState<string[]>(dropdownItems);
+  const [dropdownSelectItem, setDropdownSelectItem] = useState<string>(dropdownItems[0]);
+
+  const dropdownSelectItemHandler = (selectItem: string) => {
+    setDropdownSelectItem(selectItem);
+    setOpenDropdown(false);
+  };
+
+  const searchDropdownList = (searchItem: string) => {
+    const searchItemList = dropdownItems.filter((item) =>
+      item.toUpperCase().includes(searchItem.toUpperCase()),
+    );
+    setDropdownList(searchItemList);
+  };
+
   return (
     <CenterContainer>
-      <div>Dropdown</div>
+      <div className={styles.container}>
+        <div className={styles.dropdownBox} onClick={() => setOpenDropdown(!openDropdown)}>
+          <span>{dropdownSelectItem}</span>
+          <FontAwesomeIcon icon={faSortDown} />
+        </div>
+        {openDropdown && (
+          <div className={styles.selectItemContainer}>
+            <div className={styles.searchContainer}>
+              <FontAwesomeIcon icon={faSearch} className={styles.sesarchIcon} />
+              <input
+                type='text'
+                placeholder='Search Symbol'
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  searchDropdownList(e.currentTarget.value)
+                }
+              />
+            </div>
+            <div className={styles.selectItemBox}>
+              {dropdownList.map((item) => (
+                <div className={styles.listItemDiv}>
+                  <span className={styles.listItem} onClick={() => dropdownSelectItemHandler(item)}>
+                    {item}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </CenterContainer>
   );
 };
